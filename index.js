8e8a45d1ac1abdeddad8c3a1373857c9d9402a20
Priyesh Dwivedi <priyesh88088@gmail.com>
2025-08-14 14:43:22 +0530

    env variable set

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import router from "./Route/job.route.js"
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+
 
 const app=express();
 app.use(express.json());
@@ -8,7 +12,7 @@ app.use( "/api/v1/job",router);
 
 app.use(express.urlencoded());
 
-mongoose.connect("mongodb://127.0.0.1:27017/jobApp")
+mongoose.connect(process.env.URL) 
 .then(()=>console.log("Db connected"))
 .catch(err=>
     console.log("Error in Connecting",err)
